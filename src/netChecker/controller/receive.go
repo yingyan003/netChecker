c6a882934291f0142798f01c65f477c5ec25805d
滢光点点 <247059543@qq.com>
2018-06-30 15:36:47 +0800

    修改从redis频道中采集数据的方式

--- src/netChecker/controller/receive.go
+++ src/netChecker/controller/receive.go
@@ -13,10 +13,12 @@ import (
 	"strings"
 )
 
+//从redis订阅频道中，采集网络检测结果的数据，然后组装存入redis中
 func ReceiveAndSaveData() {
-	podToPodCh := make(chan Records)
-	podToNodeCh := make(chan Records)
-	podToSvcCh := make(chan Records)
+	termFinish := 0
+	podToPodCh := make(chan int, 1)
+	podToNodeCh := make(chan int, 1)
+	podToSvcCh := make(chan int, 1)
 
 	//通过管道传输report
 	go getDataByChannel(constant.CHANNEL_REPORT_POD, podToPodCh)
@@ -24,30 +26,24 @@ func ReceiveAndSaveData() {
 	go getDataByChannel(constant.CHANNEL_REPORT_SVC, podToSvcCh)
 
 	for {
-		var reportAll Records
+		select {
+		case <-podToSvcCh:
+			termFinish++
+		case <-podToNodeCh:
+			termFinish++
+		case <-podToPodCh:
+			termFinish++
+		default:
+			//防止程序阻塞
+		}
 
-		svcReport := <-podToSvcCh
-		log.Infof("ReceiveAndSaveData podToSvcCh return")
-		nodeReport := <-podToNodeCh
-		log.Infof("ReceiveAndSaveData podToNodeCh return")
-		podReport := <-podToPodCh
-		log.Infof("ReceiveAndSaveData podToPodCh return. getDataByChannel finish")
-
-		reportAll = append(reportAll, svcReport...)
-		reportAll = append(reportAll, nodeReport...)
-		reportAll = append(reportAll, podReport...)
-
-		reportSimp := getSimpleData(&reportAll)
-		reportNTN := getNTNDataFromPodReport(&nodeReport)
-
-		saveData(reportAll, constant.ALL)
-		saveData(reportAll, constant.FALSE)
-		saveData(reportAll, constant.TRUE)
-		saveData(reportSimp, constant.FALSE_SIMPLE)
-		saveData(reportSimp, constant.TRUE_SIMPLE)
-		saveData(reportNTN, constant.NOTE_TO_NODE)
-
-		log.Infof("ReceiveAndSaveData save all data success")
+		//3种类型的数据采集完毕：pod->pod,pod->node,pod->svc
+		if termFinish >= 3 {
+			//重置新一轮的计时
+			termFinish = 0
+			//组装数据，存入redis
+			getAndSave()
+		}
 	}
 }
 
@@ -79,14 +75,16 @@ func getNodeLength() int {
 //1. 消息接受的次数达到集群的节点数（因为节点都发布自己的检查结果）
 //2. 定时器结束。该定时器在每轮第一次收到消息后开始启动。目的在于解决某些节点发布检查结果消息失败，
 //	导致条件一无法满足。如此一来，既无法获取实际的检查结果，且导致频道中的消息堆积，无法区分最新一轮的检查结果。
-func getDataByChannel(channel string, reportCh chan Records) {
+func getDataByChannel(channel string, reportCh chan int) {
 	var nodeCount, nodeLength int
 	var subConn *redis.PubSubConn
-	receivedCh := make(chan int)
+	var newTermTime time.Time
+	newTerm := false
 	records, recordsTmp := Records{}, Records{}
 
-	timeout := time.Duration(utils.LoadEnvVarInt(constant.ENV_RECEIVE_TIMEOUT, constant.RECEIVE_TIMEOUT))
-	timer := time.NewTimer(time.Second * timeout)
+	timeout := utils.LoadEnvVarInt(constant.ENV_RECEIVE_TIMEOUT, constant.RECEIVE_TIMEOUT)
+	//todo 这里只在当前goroutine执行时初始化连接redis。当下面goroutine中的for循环在执行时，redis连接断了，
+	//todo 那么就在也连不上redis了，除非重启netgaurd的pod。后续考虑在下面的for循环中处理连接情况。
 	subConn = cache.GetSubConn(channel)
 	if subConn == nil {
 		subConn = cache.RetrySubConn(channel)
@@ -94,54 +92,77 @@ func getDataByChannel(channel string, reportCh chan Records) {
 
 	go func() {
 		for {
-			select {
-			default:
-				log.Infof("wait to ReceiveSubMessage")
-				//这里的定时器是防止订阅的频道始终无数据输出，而导致ReceiveAndSaveData函数阻塞在相应的channel中，无法处理结果
-				timer.Reset(time.Second * timeout)
-				data := cache.ReceiveSubMessage(subConn)
-				if data != nil {
-					//这里的定时是为解决当订阅频道无数据数据输出时，程序阻塞在receive处，导致结果无法采集并存储于redis中
-					timer.Reset(time.Second * timeout)
-
-					nodeCount++
-					if nodeCount == 1 {
-						//当消息成功订阅时，获取实际的节点数
-						nodeLength = getNodeLength()
-					}
-					err := json.Unmarshal(data, &recordsTmp)
-					utils.CheckError("getDataByChannel: Unmarshal failed ", err)
-					records = append(records, recordsTmp...)
-					if nodeCount == nodeLength {
-						//接受到的订阅消息数量达到节点数时，说明该轮的检查结果获取完毕
-						receivedCh <- 1
-					}
-					log.Infof("getDataByChannel sub message success. channel=%s, count=%d", channel, nodeCount)
+			//本轮数据采集结束。条件：1.是新的一轮 2.所有数据成功采集或数据采集超时
+			if newTerm && (nodeCount >= nodeLength || time.Now().Sub(newTermTime).Seconds() > float64(timeout) ) {
+				result, err := json.Marshal(records)
+				utils.CheckError("getDataByChannel: json marshal failed.", err)
+				//将采集结果存入redis，该结果只是某种类型的网络检测的全部结果，如svc/node/pod
+				cache.SetWithExpire(channel, string(result), redisExpire)
+				log.Infof("getDataByChannel: get and save all data success. channel=%s, datalen=%d, nodelen=%d", channel, nodeCount, nodeLength)
+				newTerm = false
+				nodeCount = 0
+				records = Records{}
+				//通知函数ReceiveAndSaveData()，本类型本轮数据采集完毕
+				reportCh <- 1
+			}
+			log.Infof("wait to ReceiveSubMessage")
+			data := cache.ReceiveSubMessage(subConn)
+			if data != nil {
+				nodeCount++
+				if nodeCount == 1 {
+					//当消息成功订阅时，获取实际的节点数
+					nodeLength = getNodeLength()
+					//标记新一轮的数据采集
+					newTerm = true
+					newTermTime = time.Now()
+					log.Infof("getDataByChannel: new term come. newTermTime=%s", newTermTime.String())
 				}
+				err := json.Unmarshal(data, &recordsTmp)
+				utils.CheckError("getDataByChannel: Unmarshal failed ", err)
+				records = append(records, recordsTmp...)
+				log.Infof("getDataByChannel: sub message success. channel=%s, count=%d", channel, nodeCount)
 			}
 		}
 	}()
+}
 
-	for {
-		select {
-		//本轮消息接受完毕
-		case <-receivedCh:
-			reportCh <- records
-			_, _ = json.Marshal(records)
-			log.Infof("getDataByChannel success. reason is reveive all data. channel=%s, length=%d", channel, nodeCount)
-			nodeCount = 0
-			records = Records{}
-			//当本轮数据成功接收（没有node/ping故障时），防止因超时而执行下一个case，导致空数据（成功接收时，这里将records重置为空对象）覆盖redis的网络检测结果数据
-			timer.Reset(time.Second * time.Duration(utils.LoadEnvVarInt(constant.ENV_GET_RESOURCE_TICKER, constant.GET_RESOURCE_TICKER)))
-			//本轮消息接收超时
-		case <-timer.C:
-			reportCh <- records
-			_, _ = json.Marshal(records)
-			log.Infof("getDataByChannel success. reason is timeout. channel=%s, length=%d", channel, nodeCount)
-			nodeCount = 0
-			records = Records{}
-		}
+func getAndSave() {
+	var podReport, nodeReport, svcReport, reportAll Records
+
+	dataPod := cache.Get(constant.CHANNEL_REPORT_POD)
+	dataNode := cache.Get(constant.CHANNEL_REPORT_NODE)
+	dataSvc := cache.Get(constant.CHANNEL_REPORT_SVC)
+
+	if !strings.EqualFold(dataPod, "") {
+		err := json.Unmarshal([]byte(dataPod), &podReport)
+		utils.CheckError("ReceiveAndSaveData: dataPod json Unmarshal failed.", err)
 	}
+	if !strings.EqualFold(dataNode, "") {
+		err := json.Unmarshal([]byte(dataNode), &nodeReport)
+		utils.CheckError("ReceiveAndSaveData: dataNode json Unmarshal failed.", err)
+	}
+	if !strings.EqualFold(dataSvc, "") {
+		err := json.Unmarshal([]byte(dataSvc), &svcReport)
+		utils.CheckError("ReceiveAndSaveData: dataSvc json Unmarshal failed.", err)
+	}
+
+	log.Infof("ReceiveAndSaveData: all data get success. podslen=%d, nodeslen=%d, svcslen=%d", len(podReport), len(nodeReport), len(svcReport))
+
+	reportAll = append(reportAll, svcReport...)
+	reportAll = append(reportAll, nodeReport...)
+	reportAll = append(reportAll, podReport...)
+
+	reportSimp := getSimpleData(&reportAll)
+	reportNTN := getNTNDataFromPodReport(&nodeReport)
+
+	saveData(reportAll, constant.ALL)
+	saveData(reportAll, constant.FALSE)
+	saveData(reportAll, constant.TRUE)
+	saveData(reportSimp, constant.FALSE_SIMPLE)
+	saveData(reportSimp, constant.TRUE_SIMPLE)
+	saveData(reportNTN, constant.NOTE_TO_NODE)
+
+	log.Infof("ReceiveAndSaveData save all data success")
 }
 
 func saveData(report interface{}, flag int) {
@@ -248,11 +269,15 @@ func getNTNDataFromPodReport(podReport *Records) ReportNodeToNode {
 		ntn.To = make([]NodeResult, 0)
 
 		srcIp := (*podReport)[0].From.HostIP
-		for _, pod := range *podReport {
+		for i, pod := range *podReport {
 			if strings.EqualFold(srcIp, pod.From.HostIP) {
 				ntn.SrcIp = pod.From.HostIP
 				result := getNTNResultFromPod(&pod)
 				ntn.To = append(ntn.To, *result)
+				//最后一组数据
+				if i == len(*podReport)-1 {
+					ntns = append(ntns, *ntn)
+				}
 			} else {
 				ntns = append(ntns, *ntn)
 
@@ -265,9 +290,10 @@ func getNTNDataFromPodReport(podReport *Records) ReportNodeToNode {
 				ntn.To = append(ntn.To, *result)
 			}
 		}
-		log.Infof("getNTNDataFromPodReport success。 data=%v", ntns)
+		log.Infof("getNTNDataFromPodReport success。 podReport len=%d, nodeToNodeReport len=%d", len(*podReport), len(ntns))
 		return ntns
 	}
+	log.Errorf("getNTNDataFromPodReport failed。 podReport=%v", *podReport)
 	return nil
 }
 
@@ -296,11 +322,15 @@ func getNTNDataFromNodeReport(nodeReport *Records) ReportNodeToNode {
 		ntn.To = make([]NodeResult, 0)
 
 		srcIp := (*nodeReport)[0].From.HostIP
-		for _, node := range *nodeReport {
+		for i, node := range *nodeReport {
 			if strings.EqualFold(srcIp, node.From.HostIP) {
 				ntn.SrcIp = node.From.HostIP
 				result := getNTNResultFromNode(&node)
 				ntn.To = append(ntn.To, *result)
+				//最后一组数据
+				if i == len(*nodeReport) {
+					ntns = append(ntns, *ntn)
+				}
 			} else {
 				ntns = append(ntns, *ntn)
 
