1ec44f067b8956b527aa0d43b3355b89d87fae40
滢光点点 <247059543@qq.com>
2018-06-30 15:33:48 +0800

    修改handler获取数据方式

--- src/netChecker/controller/handler.go
+++ src/netChecker/controller/handler.go
@@ -4,41 +4,58 @@ import (
 	"fmt"
 	"net/http"
 	"common/constant"
+	"common/utils"
+	"strings"
 )
 
 func True(w http.ResponseWriter, r *http.Request) {
-	data := cache.Get(constant.TRUE_DATA)
+	utils.CorsHandler(w)
+	data := getDataFromRedis(constant.TRUE_DATA)
 	log.Infof("handler True http requst success")
 	fmt.Fprintf(w, "%s\n", data)
 }
 
 func False(w http.ResponseWriter, r *http.Request) {
-	data := cache.Get(constant.FALSE_DATA)
+	utils.CorsHandler(w)
+	data := getDataFromRedis(constant.FALSE_DATA)
 	log.Infof("handler False http requst success")
 	fmt.Fprintf(w, "%s\n", data)
 }
 
 func All(w http.ResponseWriter, r *http.Request) {
-	data := cache.Get(constant.ALL_DATA)
+	utils.CorsHandler(w)
+	data := getDataFromRedis(constant.ALL_DATA)
 	log.Infof("handler All http requst success")
 	fmt.Fprintf(w, "%s\n", data)
 }
 
 func TrueSimple(w http.ResponseWriter, r *http.Request) {
-	data := cache.Get(constant.SIMPLE_TRUE_DATA)
+	utils.CorsHandler(w)
+	data := getDataFromRedis(constant.SIMPLE_TRUE_DATA)
 	log.Infof("handler TrueSimple http requst success")
 	fmt.Fprintf(w, "%s\n", data)
 }
 
 func FalseSimple(w http.ResponseWriter, r *http.Request) {
-	data := cache.Get(constant.SIMPLE_FALSE_DATA)
+	utils.CorsHandler(w)
+	data := getDataFromRedis(constant.SIMPLE_FALSE_DATA)
 	log.Infof("handler FalseSimple http requst success")
 	fmt.Fprintf(w, "%s\n", data)
 }
 
 func NodeToNode(w http.ResponseWriter, r *http.Request) {
-	data := cache.Get(constant.NOTE_TO_NODE_DATA)
+	utils.CorsHandler(w)
+	data := getDataFromRedis(constant.NOTE_TO_NODE_DATA)
 	log.Infof("handler NodeToNode http requst success")
 	fmt.Fprintf(w, "%s\n", data)
 }
 
+func getDataFromRedis(tp string) string {
+	data := cache.Get(tp)
+	if strings.EqualFold(data, "") {
+		log.Infof("handler getDataFromRedis: request %s data = null. exec func getAndSave()", tp)
+		getAndSave()
+		data = cache.Get(tp)
+	}
+	return data
+}
